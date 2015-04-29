f795a43464cd02a4b16565d2080c750a62eafaa6
robertleec <libinbin@libinbins-Mac-mini.local>
2015-04-29 14:04:56 +0800

    Refactor element type with template.

--- Matrix.h
+++ Matrix.h
@@ -6,29 +6,66 @@
 
 namespace std {
     
-    typedef double ElementType;
-    
     typedef unsigned long SizeType;
     typedef double MultipleNumberType;
     typedef unsigned long RankValueType;
     typedef double TraceValueType;
     
+    template<typename ElementType>
     class Matrix {
         
     public:
-        Matrix();
+        Matrix() {
+            
+        }
         
-        Matrix(const Matrix& rhs);
-        Matrix& operator=(const Matrix& rhs);
+        Matrix(const Matrix& rhs):data(rhs.data) {
+            
+        }
         
-        Matrix(SizeType rowNumber, SizeType columNumber);
+        Matrix& operator=(const Matrix& rhs) {
+            Matrix temp(rhs);
+            this->swap(temp);
+            return *this;
+        }
         
-        ~Matrix();
+        Matrix(SizeType rowNumber, SizeType columNumber) {
+            if (rowNumber == 0 || columNumber == 0) {
+                return;
+            }
+            
+            for (size_t index = 0; index < rowNumber; ++index) {
+                vector<ElementType> row(columNumber);
+                this->data.push_back(row);
+            }
+        }
         
-        void swap(Matrix& other);
+        ~Matrix() {
+            
+        }
         
-        bool isEmpty() const;
-        bool isSquareMatrix() const;
+        void swap(Matrix& other) {
+            using std::swap;
+            swap(this->data, other.data);
+        }
+        
+        bool isEmpty() const {
+            
+            if (this->getRowNumber() > 0 && this->getColumNumber() > 0) {
+                return false;
+            }
+            
+            return true;
+        }
+        
+        bool isSquareMatrix() const {
+            
+            if (this->getRowNumber() == this->getColumNumber()) {
+                return true;
+            }
+            
+            return false;
+        }
         
         inline const SizeType getRowNumber() const {
             return (this->data).size();
@@ -66,11 +103,59 @@ namespace std {
             return vector<ElementType>(colum);
         }
         
-        void addRow(const vector<ElementType>& rowData);
-        void deleteRow(SizeType rowIndex);
+        void addRow(const vector<ElementType>& rowData) {
+            
+            if (this->getRowNumber() == 0) {
+                
+                this->data.push_back(rowData);
+                
+            } else {
+                
+                assert(rowData.size() == this->getColumNumber());
+                this->data.push_back(rowData);
+            }
+        }
         
-        void addColum(const vector<ElementType>& columData);
-        void deleteColum(SizeType columIndex);
+        void deleteRow(SizeType rowIndex) {
+            assert(this->getRowNumber() > rowIndex);
+            
+            typename vector< vector<ElementType> >::iterator deleteIterator = this->data.begin() + rowIndex;
+            
+            this->data.erase(deleteIterator);
+        }
+        
+        void addColum(const vector<ElementType>& columData) {
+            
+            if (this->getRowNumber() == 0) {
+                for (size_t index = 0; index < columData.size(); ++index) {
+                    vector<ElementType> row(1, columData.at(index));
+                    this->data.push_back(row);
+                }
+            } else {
+                
+                assert(columData.size() == this->getRowNumber());
+                
+                for (size_t index = 0; index < columData.size(); ++index) {
+                    this->data.at(index).push_back(columData.at(index));
+                }
+            }
+        }
+        
+        void deleteColum(SizeType columIndex) {
+            
+            assert(this->getColumNumber() > columIndex);
+            
+            if (this->getColumNumber() == 1 && columIndex == 0) {
+                this->data.clear();
+            } else {
+                
+                for (size_t index = 0; index < this->getRowNumber(); ++index) {
+                    vector<ElementType>& row = this->data.at(index);
+                    typename vector<ElementType>::iterator deleteIterator = row.begin() + columIndex;
+                    row.erase(deleteIterator);
+                }
+            }
+        }
         
         friend inline const Matrix operator+(const Matrix& lhs, const Matrix& rhs) {
             
@@ -145,13 +230,72 @@ namespace std {
             return Matrix(result);
         }
         
-        void switchTwoRow(SizeType rowIndex0, SizeType rowIndex1);
-        void multipleRow(MultipleNumberType multipleNumber, SizeType rowIndex);
-        void addOneMultipleRowToAnotherRow(SizeType rowIndex0, MultipleNumberType multipleNumber, SizeType rowIndex1);
+        void switchTwoRow(SizeType rowIndex0, SizeType rowIndex1) {
+            
+            assert(this->getRowNumber() > rowIndex0 && this->getRowNumber() > rowIndex1);
+            
+            using std::swap;
+            swap(this->data.at(rowIndex0), this->data.at(rowIndex1));
+        }
         
-        void switchTwoColum(SizeType columIndex0, SizeType columIndex1);
-        void multipleColum(MultipleNumberType multipleNumber, SizeType columIndex);
-        void addOneMultipleColumToAnotherColum(SizeType columIndex0, MultipleNumberType multipleNumber, SizeType columindex1);
+        void multipleRow(MultipleNumberType multipleNumber, SizeType rowIndex) {
+            
+            assert(this->getRowNumber() > rowIndex);
+            
+            vector<ElementType>& row = this->data.at(rowIndex);
+            
+            for (size_t index = 0; index < row.size(); ++index) {
+                row.at(index) *= multipleNumber;
+            }
+        }
+        
+        void addOneMultipleRowToAnotherRow(SizeType rowIndex0, MultipleNumberType multipleNumber, SizeType rowIndex1) {
+            
+            assert(this->getRowNumber() > rowIndex0 && this->getRowNumber() > rowIndex1);
+            
+            const vector<ElementType>& row0 = this->data.at(rowIndex0);
+            vector<ElementType>& row1 = this->data.at(rowIndex1);
+            
+            for (size_t index = 0; index < row0.size(); ++index) {
+                row1.at(index) += row0.at(index) * multipleNumber;
+            }
+        }
+        
+        void switchTwoColum(SizeType columIndex0, SizeType columIndex1) {
+            
+            assert(this->getColumNumber() > columIndex0 && this->getColumNumber() > columIndex1);
+            
+            using std::swap;
+            
+            for (size_t index = 0; index < this->getRowNumber(); ++index) {
+                
+                vector<ElementType>& row = this->data.at(index);
+                swap(row.at(columIndex0), row.at(columIndex1));
+            }
+        }
+        
+        void multipleColum(MultipleNumberType multipleNumber, SizeType columIndex) {
+            
+            assert(this->getColumNumber() > columIndex);
+            
+            for (size_t index = 0; index < this->getRowNumber(); ++index) {
+                
+                vector<ElementType>& row = this->data.at(index);
+                row.at(columIndex) *= multipleNumber;
+            }
+        }
+        
+        void addOneMultipleColumToAnotherColum(SizeType columIndex0, MultipleNumberType multipleNumber, SizeType columIndex1) {
+            
+            assert(this->getColumNumber() > columIndex0 && this->getColumNumber() > columIndex1);
+            
+            for (size_t index = 0; index < this->getRowNumber(); ++index) {
+                
+                vector<ElementType>& row = this->data.at(index);
+                
+                row.at(columIndex1) += row.at(columIndex0) * multipleNumber;
+            }
+        }
         
         inline const Matrix inverseOfSquareMatrix() {
             
@@ -191,8 +335,48 @@ namespace std {
             return Matrix(result);
         }
         
-        RankValueType getRank();
-        TraceValueType getTrace();
+        RankValueType getRank() {
+            
+            Matrix matrixCopy(*this);
+            
+            RankValueType rank = 0;
+            
+            for (size_t rowIndex = 0; rowIndex < matrixCopy.getRowNumber(); ++rowIndex) {
+                
+                SizeType firstColumNonZero = matrixCopy.firstColumNonZeroElementUpRow(rowIndex);
+                
+                //Not found non-zero element up row index.
+                if (firstColumNonZero == matrixCopy.getColumNumber()) {
+                    break;
+                }
+                
+                ++rank;
+                
+                SizeType firstRowNonZeroInColum = matrixCopy.firstRowContainsNonZeroElementColumUpRow(firstColumNonZero, rowIndex);
+                
+                matrixCopy.switchTwoRow(rowIndex, firstRowNonZeroInColum);
+                
+                for (size_t eliminationIndex = rowIndex + 1; eliminationIndex < matrixCopy.getRowNumber(); ++eliminationIndex) {
+                    
+                    matrixCopy.addOneMultipleRowToAnotherRow(rowIndex, (-1) * (matrixCopy.data.at(eliminationIndex).at(firstColumNonZero) / matrixCopy.data.at(rowIndex).at(firstColumNonZero)), eliminationIndex);
+                }
+            }
+            
+            return rank;
+        }
+        
+        TraceValueType getTrace() {
+            
+            assert(this->isSquareMatrix() == true);
+            
+            TraceValueType trace = 0;
+            
+            for (size_t rowIndex = 0; rowIndex < this->getRowNumber(); ++rowIndex) {
+                trace += this->data.at(rowIndex).at(rowIndex);
+            }
+            
+            return trace;
+        }
         
         static inline const Matrix unitaryMatrix(SizeType size) {
             
@@ -213,15 +397,54 @@ namespace std {
     private:
         vector< vector<ElementType> > data;
         
-        SizeType firstRowContainsNonZeroElementColumUpRow(SizeType columIndex, SizeType upRowIndex);
-        SizeType firstColumNonZeroElementUpRow(SizeType upRowIndex);
+        SizeType firstRowContainsNonZeroElementColumUpRow(SizeType columIndex, SizeType upRowIndex) {
+            
+            assert(this->getColumNumber() > columIndex);
+            assert(this->getRowNumber() > upRowIndex);
+            
+            SizeType firstRowIndex = this->getRowNumber();
+            
+            for (size_t rowIndex = upRowIndex; rowIndex < this->getRowNumber(); ++rowIndex) {
+                
+                if (this->data.at(rowIndex).at(columIndex) != 0) {
+                    firstRowIndex = rowIndex;
+                    break;
+                }
+            }
+            
+            return firstRowIndex;
+        }
+        
+        SizeType firstColumNonZeroElementUpRow(SizeType upRowIndex) {
+            
+            assert(this->getRowNumber() > upRowIndex);
+            
+            SizeType firstColumIndex = this->getColumNumber();
+            
+            for (size_t rowIndex = upRowIndex; rowIndex < this->getRowNumber(); ++rowIndex) {
+                
+                vector<ElementType>& row = this->data.at(rowIndex);
+                
+                for (size_t columIndex = 0; columIndex < row.size(); ++columIndex) {
+                    
+                    if (row.at(columIndex) != 0 && firstColumIndex > columIndex) {
+                        firstColumIndex = columIndex;
+                        break;
+                    }
+                }
+            }
+            
+            return firstColumIndex;
+        }
     };
     
 }
 
 namespace std {
-    template<>
-    void swap<std::Matrix>(std::Matrix& a, std::Matrix& b);
+    template<typename ElementType>
+    void swap(std::Matrix<ElementType>& a, std::Matrix<ElementType>& b) {
+        a.swap(b);
+    }
 }
 
 #endif
--- demo.cpp
+++ demo.cpp
@@ -4,51 +4,51 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     
-    Matrix matrixA;
+    Matrix<double> matrixA;
     
-    ElementType arrayA1[] = {1, 0, 3, -1};
-    vector<ElementType> vectorA1(arrayA1, arrayA1 + 4);
+    double arrayA1[] = {1, 0, 3, -1};
+    vector<double> vectorA1(arrayA1, arrayA1 + 4);
     matrixA.addRow(vectorA1);
     
-    ElementType arrayA2[] = {2, 1, 0, 2};
-    vector<ElementType> vectorA2(arrayA2, arrayA2 + 4);
+    double arrayA2[] = {2, 1, 0, 2};
+    vector<double> vectorA2(arrayA2, arrayA2 + 4);
     matrixA.addRow(vectorA2);
     
-    Matrix matrixB;
+    Matrix<double> matrixB;
     
-    ElementType arrayB1[] = {4, 1, 0};
-    vector<ElementType> vectorB1(arrayB1, arrayB1 + 3);
+    double arrayB1[] = {4, 1, 0};
+    vector<double> vectorB1(arrayB1, arrayB1 + 3);
     matrixB.addRow(vectorB1);
     
-    ElementType arrayB2[] = {-1, 1, 3};
-    vector<ElementType> vectorB2(arrayB2, arrayB2 + 3);
+    double arrayB2[] = {-1, 1, 3};
+    vector<double> vectorB2(arrayB2, arrayB2 + 3);
     matrixB.addRow(vectorB2);
     
-    ElementType arrayB3[] = {2, 0, 1};
-    vector<ElementType> vectorB3(arrayB3, arrayB3 + 3);
+    double arrayB3[] = {2, 0, 1};
+    vector<double> vectorB3(arrayB3, arrayB3 + 3);
     matrixB.addRow(vectorB3);
     
-    ElementType arrayB4[] = {1, 3, 4};
-    vector<ElementType> vectorB4(arrayB4, arrayB4 + 3);
+    double arrayB4[] = {1, 3, 4};
+    vector<double> vectorB4(arrayB4, arrayB4 + 3);
     matrixB.addRow(vectorB4);
     
-    Matrix matrixC = matrixA * matrixB;
+    Matrix<double> matrixC = matrixA * matrixB;
     
-    Matrix matrix;
+    Matrix<double> matrix;
     
-    ElementType array1[] = {1, 0, 1};
-    vector<ElementType> vector1(array1, array1 + 3);
+    double array1[] = {1, 0, 1};
+    vector<double> vector1(array1, array1 + 3);
     matrix.addRow(vector1);
     
-    ElementType array2[] = {2, 1, 0};
-    vector<ElementType> vector2(array2, array2 + 3);
+    double array2[] = {2, 1, 0};
+    vector<double> vector2(array2, array2 + 3);
     matrix.addRow(vector2);
     
-    ElementType array3[] = {-3, 2, -5};
-    vector<ElementType> vector3(array3, array3 + 3);
+    double array3[] = {-3, 2, -5};
+    vector<double> vector3(array3, array3 + 3);
     matrix.addRow(vector3);
     
-    Matrix inverseMatrix = matrix.inverseOfSquareMatrix();
+    Matrix<double> inverseMatrix = matrix.inverseOfSquareMatrix();
     
     return 0;
 }
